de21c3fafeea0ce4bac29b74e421148317342570
Michal Ajduk <michal.ajduk@canonical.com>
2022-01-05 12:52:38 +0100

    Fixed installer

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -x
 [ -d foo ] || mkdir work
-
+sudo apt update
 sudo apt install -y mpd
 sudo cp src/mpd.conf /etc/mpd.conf
 sudo cp src/mpd.service /etc/systemd/system/
@@ -19,8 +19,9 @@ sudo systemctl start gpiod
 
 
 wget https://github.com/Spotifyd/spotifyd/releases/download/v0.3.3/spotifyd-linux-armv6-slim.tar.gz -O work/spotifyd-linux-armv6-slim.tar.gz
-tar -xzvf work/spotifyd-linux-armv6-slim.tar.gz
+tar -xzvf work/spotifyd-linux-armv6-slim.tar.gz work/
 chmod 755 work/spotifyd
+mkdir -p /home/pi/.config/spotifyd
 touch /home/pi/.config/spotifyd/spotifyd.conf
 sudo cp src/spotifyd.service /etc/systemd/system/
 sudo chown root:root /etc/systemd/system/spotifyd.service
