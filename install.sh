5eb60d36e166dc4e0acf972f3cc0b974eb9ce551
Michal Ajduk <michal.ajduk@canonical.com>
2022-01-31 11:50:50 +0100

    Refactor config management

--- install.sh
+++ install.sh
@@ -1,99 +1,165 @@
 #!/usr/bin/env bash
-set -x
 
-[ -d foo ] || mkdir work
-sudo apt update
-sudo apt install -y python3-pip
-sudo apt install -y bridge-utils
-# Installing PulseAudio
-sudo apt install -y --no-install-recommends pulseaudio
-sudo usermod -a -G pulse-access root
-sudo mv /etc/pulse/client.conf /etc/pulse/client.conf.orig
-sudo cp src/pulse/client.conf /etc/pulse/client.conf
-sudo cp src/pulse/daemon.conf /etc/pulse/daemon.conf
-sudo cp src/pulse/default.pa /etc/pulse/default.pa
-sudo cp src/pulse/system.pa /etc/pulse/system.pa
-sudo cp src/pulseaudio.service /etc/systemd/system/pulseaudio.service
-sudo chown root:root /etc/systemd/system/pulseaudio.service
-sudo mv /etc/alsa/conf.d/50-pulseaudio.conf{,.orig}
-sudo mv /etc/alsa/conf.d/99-pulse.conf{,.orig}
-sudo cp src/99-pulseaudio.conf /etc/alsa/conf.d/99-pulseaudio.conf
-sudo chown root:root /etc/alsa/conf.d/99-pulseaudio.conf
-sudo systemctl enable --now pulseaudio.service
-sudo systemctl --global mask pulseaudio.socket
-sudo pip3 install pulsectl
+[ -d foo ] || mkdir -p work
 
-# Setting up bluetooth
-sudo apt install -y --no-install-recommends bluez-tools pulseaudio-module-bluetooth
-sudo cp src/bluetooth/main.conf /etc/bluetooth/main.conf
-sudo mkdir -p /etc/systemd/system/bthelper@.service.d
-sudo cp src/bluetooth/override.conf /etc/systemd/system/bthelper@.service.d/override.conf
-sudo chown root:root /etc/systemd/system/bthelper@.service.d/override.conf
-sudo cp src/bluetooth/bt-agent@.service /etc/systemd/system/bt-agent@.service
-sudo chown root:root /etc/systemd/system/bt-agent@.service
-systemctl daemon-reload
-systemctl enable bt-agent@hci0.service
-sudo usermod -a -G bluetooth pulse
+install_base () {
+  sudo apt update
+  sudo apt install -y python3-pip
+  sudo apt install -y bridge-utils
+  curl -sSL https://get.docker.com | sh
+  sudo usermod -aG docker ${USER}
+  sudo systemctl enable docker
+}
 
-# Installing MPD
-sudo apt install -o Dpkg::Options::="--force-confold" -y mpd
-sudo apt install -y mpc
-sudo usermod -a -G pulse-access mpd
-sudo cp src/mpd.conf /etc/mpd.conf
-sudo cp src/mpd.service /etc/systemd/system/
-sudo chown root:root /etc/systemd/system/mpd.service
-sudo systemctl daemon-reload
-sudo systemctl enable mpd
-sudo systemctl start mpd
-sudo cp src/rp_crontab /etc/cron.d/rp_crontab
-sudo chown root:root /etc/cron.d/rp_crontab
-sudo systemctl restart cron
-sudo cp src/radio.mpl.m3u /var/lib/mpd/playlists/radio.mpl.m3u
-sudo chown mpd:audio /var/lib/mpd/playlists/radio.mpl.m3u
+install_pulseaudio () {
+  sudo apt install -y --no-install-recommends pulseaudio
+  sudo usermod -a -G pulse-access root
+  sudo pip3 install pulsectl
+  curl https://raw.githubusercontent.com/GeorgeFilipkin/pulsemixer/master/pulsemixer > ./pulsemixer && chmod +x ./pulsemixer
+  configure_pulseaudio
+}
 
-# Installing GPIO
-sudo cp src/gpiod.service /etc/systemd/system/
-sudo chown root:root /etc/systemd/system/gpiod.service
-sudo systemctl daemon-reload
-sudo systemctl enable gpiod
-sudo systemctl start gpiod
+configure_pulseaudio () {
+  sudo cp etc/pulse/client.conf /etc/pulse/client.conf
+  sudo cp etc/pulse/daemon.conf /etc/pulse/daemon.conf
+  sudo cp etc/pulse/default.pa /etc/pulse/default.pa
+  sudo cp etc/pulse/system.pa /etc/pulse/system.pa
+  sudo cp etc/systemd/system/pulseaudio.service /etc/systemd/system/pulseaudio.service
+  sudo chown root:root /etc/systemd/system/pulseaudio.service
+  sudo cp etc/alsa/conf.d/99-pulseaudio.conf /etc/alsa/conf.d/99-pulseaudio.conf
+  sudo chown root:root /etc/alsa/conf.d/99-pulseaudio.conf
+  sudo systemctl enable --now pulseaudio.service
+  sudo systemctl --global mask pulseaudio.socket
+}
 
-# Installing Spotifyd
-wget https://github.com/Spotifyd/spotifyd/releases/download/v0.3.3/spotifyd-linux-armv6-slim.tar.gz -O work/spotifyd-linux-armv6-slim.tar.gz
-tar -xzvf work/spotifyd-linux-armv6-slim.tar.gz work/
-chmod 755 work/spotifyd
-mkdir -p /home/pi/.config/spotifyd
-touch /home/pi/.config/spotifyd/spotifyd.conf
-cp src/spotifyd.conf /home/pi/.config/spotifyd/spotifyd.conf
-sudo cp src/spotifyd.service /etc/systemd/system/
-sudo chown root:root /etc/systemd/system/spotifyd.service
-sudo systemctl daemon-reload
-sudo systemctl enable spotifyd
-sudo systemctl start spotifyd
+install_bluetooth () {
+  sudo apt install -y --no-install-recommends bluez-tools pulseaudio-module-bluetooth
+  sudo usermod -a -G bluetooth pulse
+  configure_bluetooth
+}
 
-# Installing Pulsemixer
-curl https://raw.githubusercontent.com/GeorgeFilipkin/pulsemixer/master/pulsemixer > pulsemixer && chmod +x ./pulsemixer
+configure_bluetooth () {
+  sudo cp etc/bluetooth/main.conf /etc/bluetooth/main.conf
+  sudo mkdir -p /etc/systemd/system/bthelper@.service.d
+  sudo cp etc/systemd/system/bthelper@.service.d/override.conf /etc/systemd/system/bthelper@.service.d/override.conf
+  sudo chown root:root /etc/systemd/system/bthelper@.service.d/override.conf
+  sudo cp etc/systemd/system/bt-agent@.service /etc/systemd/system/bt-agent@.service
+  sudo chown root:root /etc/systemd/system/bt-agent@.service
+  systemctl daemon-reload
+  systemctl enable bt-agent@hci0.service
+}
 
-# Minimalistic http server
-sudo cp src/http-server.service /etc/systemd/system/
-sudo chown root:root /etc/systemd/system/http-server.service
-cp src/projector.py work/
-cp src/server.py work/
-sudo systemctl daemon-reload
-sudo systemctl enable http-server
-sudo systemctl start http-server
+install_mpd () {
+  sudo apt install -o Dpkg::Options::="--force-confold" -y mpd
+  sudo apt install -y mpc
+  sudo usermod -a -G pulse-access mpd
+  configure_mpd
+}
 
-# Installing Docker
-curl -sSL https://get.docker.com | sh
-sudo usermod -aG docker ${USER}
-sudo systemctl enable docker
+configure_mpd () {
+  sudo cp etc/mpd.conf /etc/mpd.conf
+  sudo cp etc/systemd/system/mpd.service /etc/systemd/system/mpd.service
+  sudo chown root:root /etc/systemd/system/mpd.service
+  sudo systemctl daemon-reload
+  sudo systemctl enable mpd
+  sudo systemctl start mpd
+  sudo cp etc/cron.d/rp_crontab /etc/cron.d/rp_crontab
+  sudo chown root:root /etc/cron.d/rp_crontab
+  sudo systemctl restart cron
+  sudo cp src/radio.mpl.m3u /var/lib/mpd/playlists/radio.mpl.m3u
+  sudo chown mpd:audio /var/lib/mpd/playlists/radio.mpl.m3u
+}
 
-# Instal HomeAssistant
-docker run -d \
-  --name homeassistant \
-  --privileged \
-  --restart=unless-stopped \
-  -e TZ=Europe/Warsaw \
-  -v /home/pi/raspberry-media-center/homeassistant:/config \
-  --network=host \
-  ghcr.io/home-assistant/home-assistant:stable
+configure_gpiod () {
+  sudo cp etc/systemd/system/gpiod.service /etc/systemd/system/gpiod.service
+  sudo chown root:root /etc/systemd/system/gpiod.service
+  sudo systemctl daemon-reload
+  sudo systemctl enable gpiod
+  sudo systemctl start gpiod
+}
+
+install_spotifyd () {
+  wget https://github.com/Spotifyd/spotifyd/releases/download/v0.3.3/spotifyd-linux-armv6-slim.tar.gz -O work/spotifyd-linux-armv6-slim.tar.gz
+  tar -xzvf work/spotifyd-linux-armv6-slim.tar.gz work/
+  chmod 755 work/spotifyd
+  configure_spotifyd
+}
+
+configure_spotifyd () {
+  mkdir -p /home/pi/.config/spotifyd
+  if [ ! -f /home/pi/.config/spotifyd/spotifyd.conf ]; then
+    cp src/spotifyd.conf /home/pi/.config/spotifyd/spotifyd.conf
+    echo "WARNING: Udate username and password in /home/pi/.config/spotifyd/spotifyd.conf"
+  fi
+  sudo cp etc/systemd/system/spotifyd.service /etc/systemd/system/spotifyd.service
+  sudo chown root:root /etc/systemd/system/spotifyd.service
+  sudo systemctl daemon-reload
+  sudo systemctl enable spotifyd
+  sudo systemctl start spotifyd
+}
+
+configure_httpd () {
+  sudo cp etc/systemd/system/http-server.service /etc/systemd/system/http-server.service
+  sudo chown root:root /etc/systemd/system/http-server.service
+  cp src/projector.py work/
+  cp src/server.py work/
+  sudo systemctl daemon-reload
+  sudo systemctl enable http-server
+  sudo systemctl start http-server
+}
+
+install_homeassistant () {
+  docker run -d \
+    --name homeassistant \
+    --privileged \
+    --restart=unless-stopped \
+    -e TZ=Europe/Warsaw \
+    -v /home/pi/raspberry-media-center/homeassistant:/config \
+    --network=host \
+    ghcr.io/home-assistant/home-assistant:stable
+}
+
+diff_config () {
+  for f in $( find etc/ -type f ); do
+    diff -q ${f} /${f}
+  done
+}
+
+push_config () {
+  for f in $( find etc/ -type f ); do
+    cp ${f} /${f}
+  done
+}
+
+pull_config () {
+  for f in $( find etc/ -type f ); do
+    cp /${f} ${f}
+  done
+}
+
+print_usage () {
+  echo "USAGE: $0 [-a] | [-o <step>]"
+}
+
+while getopts ":hao:" arg; do
+  case ${arg} in
+    a)
+      install_base
+      install_pulseaudio
+      install_bluetooth
+      install_mpd
+      configure_gpiod
+      install_spotifyd
+      configure_httpd
+      install_homeassistant
+      ;;
+    o)
+      f=$OPTARG
+      $f
+      ;;
+    h | *)
+      print_usage
+      ;;
+  esac
+done
+
